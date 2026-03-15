54c9032f5110093b2c68e669dcd3047896fdd14c
wasuthonson <wasuthon.son@spumail.net>
2026-03-15 09:59:53 +0700

    Add files via upload

---
+++ Lab7-68094957/mysite/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>หน้าแรกของเว็บไซต์วิชา ICT12367</h1>") 
+
+def about(request):
+    return HttpResponse("<h1>เกี่ยวกับเรา</h1>") 
+
+def form(request):
+
+    return render(request, 'form.html') 
+
+def contact(request):
+
+    return HttpResponse("<h1>ติดต่อ นายวสุธร ทรงสวัสดิ์วงศ์ รหัสนักศึกษาของคุณ</h1>")
